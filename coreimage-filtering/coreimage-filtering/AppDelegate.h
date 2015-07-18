5e66cca067d518fdcafefd6407f61c706f1d9c71
Sergey Buravtsov <serobur@gmail.com>
2015-07-18 16:35:23 +0300

    Initial commit

---
+++ coreimage-filtering/coreimage-filtering/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  coreimage-filtering
+//
+//  Created by Sergey Buravtsov on 18.07.15.
+//  Copyright (c) 2015 Sergey Buravtsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ coreimage-filtering/coreimage-filtering/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coreimage-filtering
+//
+//  Created by Sergey Buravtsov on 18.07.15.
+//  Copyright (c) 2015 Sergey Buravtsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
